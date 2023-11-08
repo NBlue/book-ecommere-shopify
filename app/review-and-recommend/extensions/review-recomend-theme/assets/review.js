765f8ba8e12a16be954913115981f96830b7a572
Sotatek-NamNguyen14 <nam.nguyen14@sotatek.com>
2023-11-08 14:01:53 +0700

    feat: add server review

--- app/review-and-recommend/extensions/review-recomend-theme/assets/review.js
+++ app/review-and-recommend/extensions/review-recomend-theme/assets/review.js
@@ -8,4 +8,22 @@ form.addEventListener("submit", (event) => {
 
   const review = form.querySelector("#review");
   console.log("FILE REVIEWER", review.value);
+
+  const formData = new FormData();
+  formData.append("image_url", fileImage.files[0]); // Chỉ lấy tệp tin đầu tiên
+  formData.append("review", review.value);
+  console.log("form data", formData);
+
+  fetch("http://localhost:9000/v1/review", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.json()) // Chuyển đổi phản hồi thành JSON
+    .then((data) => {
+      // Xử lý dữ liệu từ API backend (nếu cần)
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
--- backend/src/controllers/reviewControler.ts
+++ backend/src/controllers/reviewControler.ts
@@ -1,4 +1,6 @@
-var connection = require('../config/database');
+const fs = require('fs');
+const connection = require('../config/database');
+const cloudinary = require('../config/cloudinary');
 
 const reviewControler = {
   getAllReview: async (req: any, res: any) => {
@@ -9,7 +11,125 @@ const reviewControler = {
         return res.status(200).json({ success: true, data: results });
       });
     } catch (error) {
-      console.log('ERROR', error);
+      return res
+        .status(500)
+        .json({ success: false, error: 'Internal Server Error' });
+    }
+  },
+
+  getReviewByBook: async (req: any, res: any) => {
+    try {
+      const handle = req.params.handle;
+      const query = `SELECT * FROM reviews WHERE handle = '${handle}'`;
+      connection.query(query, (error: any, results: any) => {
+        if (error) console.log('ERROR', error);
+        return res.status(200).json({ success: true, data: results });
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ success: false, error: 'Internal Server Error' });
+    }
+  },
+
+  addReview: async (req: any, res: any) => {
+    try {
+      let query = null;
+      const email = req.body.email;
+      const userId = req.body.userId;
+      const rating = req.body.rating;
+      const comment = req.body.comment;
+      const handle = req.body.handle;
+
+      if (!req.file) {
+        query = `INSERT INTO reviews (email, userId, rating, comment, handle) VALUES ('${email}', '${userId}', ${rating}, '${comment}', '${handle}')`;
+      } else {
+        const result = await cloudinary.uploader.upload(req.file.path, {
+          folder: 'Reviews',
+        });
+        fs.unlinkSync(req.file.path);
+        const image_url = result.secure_url;
+        query = `INSERT INTO reviews (email, userId, rating, comment, image_url, handle) VALUES ('${email}', '${userId}', ${rating}, '${comment}', '${image_url}',  '${handle}')`;
+      }
+
+      connection.query(query, (error: any, results: any) => {
+        if (error)
+          return res.status(400).json({
+            success: false,
+            message: 'INSERT INTO reviews errors',
+            error: error,
+          });
+        return res
+          .status(200)
+          .json({ success: true, message: 'Add review successfully!' });
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ success: false, error: 'Internal Server Error' });
+    }
+  },
+
+  updateReview: async (req: any, res: any) => {
+    try {
+      const id = req.params.id;
+      let dataUpdate = Object.entries(req.body)
+        .map(([key, value]) => {
+          if (key === 'image_url') return null;
+          if (key === 'rating') return `${key} = ${value}`;
+          return `${key} = '${value}'`;
+        })
+        .join(', ');
+
+      if (!!req.file) {
+        const result = await cloudinary.uploader.upload(req.file.path, {
+          folder: 'Reviews',
+        });
+        const image_url = result.secure_url;
+        fs.unlinkSync(req.file.path);
+        dataUpdate += `, image_url = '${image_url}'`;
+      }
+
+      const query = `UPDATE reviews SET ${dataUpdate} WHERE id = ${id};`;
+
+      connection.query(query, (error: any, results: any) => {
+        if (error)
+          return res.status(400).json({
+            success: false,
+            message: 'Update reviews errors',
+            error: error,
+          });
+        return res
+          .status(200)
+          .json({ success: true, message: 'Update review successfully!' });
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ success: false, error: 'Internal Server Error' });
+    }
+  },
+
+  deleteReview: async (req: any, res: any) => {
+    try {
+      const id = req.params.id;
+      const query = `DELETE FROM reviews WHERE id = ${id}`;
+
+      connection.query(query, (error: any, results: any) => {
+        if (error)
+          return res.status(400).json({
+            success: false,
+            message: 'DELETE reviews record errors',
+            error: error,
+          });
+        return res
+          .status(200)
+          .json({ success: true, message: 'Delete review successfully!' });
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ success: false, error: 'Internal Server Error' });
     }
   },
 };
