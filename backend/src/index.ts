191c82bac9b212ad7c98006940e220fa0d3831c9
Sotatek-NamNguyen14 <nam.nguyen14@sotatek.com>
2023-11-08 08:23:11 +0700

    feat: review and recommend ui

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,16 +2,18 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import dotenv from 'dotenv';
 
+const cors = require('cors');
+const reviewRoute = require('./routes/reviewRoute');
+
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Well done!');
-});
+app.use('/v1/review', reviewRoute);
 
 app.listen(process.env.PORT, () => {
-  console.log('The application is listening on port 3000!');
+  console.log('The application is listening on port 9000!');
 });
